78edbe8cef5e82f4172ea96132d17ce88f692aa4
Naninara <nmvmanikanta@gmail.com>
2023-07-28 22:54:57 +0530

    about changed

--- src/Components/about.js
+++ src/Components/about.js
@@ -1,4 +1,9 @@
 const About = () => {
-  return <h2 className="context">About Us page</h2>;
+  return (
+    <>
+      <h2 className="context">About Us page</h2>
+      <h2 className="context">About Us page</h2>
+    </>
+  );
 };
 export default About;
